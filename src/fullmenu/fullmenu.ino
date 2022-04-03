8bf9ebaf6088f30e6fc5ef5002f57f4c9718fb79
Arakio34 <ttueleau@gmail.com>
2022-04-03 17:14:24 +0200

    changing menu

--- src/fullmenu/fullmenu.ino
+++ src/fullmenu/fullmenu.ino
@@ -7,13 +7,44 @@
 #define SCREEN_HEIGHT 32 
 #define OLED_RESET     4 
 #define SCREEN_ADDRESS 0x3C 
+#define OK 38
+#define LEFT 39
+#define RIGHT 37
 #define NB_MAX_CAPTEUR  3 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
+struct bValue{
+	int vleft;
+	int vok;
+	int vright;
+};
+
+struct bValue buttons;
+
 
 int listCapteur[NB_MAX_CAPTEUR];
 int actualMenu = 0;
+
+void readbValue(struct bValue * b){
+	b->vleft = digitalRead(LEFT);
+	b->vok = digitalRead(OK);
+	b->vright = digitalRead(RIGHT);
+}
+
+void printbValue(struct bValue * b){
+	Serial.println(b->vleft);
+	Serial.println(b->vok);
+	Serial.println(b->vright);
+}
+
+void setupButton(){
+  pinMode(OK,INPUT);
+  pinMode(LEFT,INPUT);
+  pinMode(RIGHT,INPUT);
+
+}
+
 
 //Scan les capteur brancher et retourne leur nombre 
 int scanCapteur(){
@@ -43,24 +74,12 @@ if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
   return 0;
 }
 
-int  creditMenu() {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(F("Done by :"));
-  display.setCursor(0, 10);
-  display.println(F("T.T. A.M. T.C."));
-  display.setCursor(10, 20);
-  display.println(F("2021/2022")); 
-  display.display();      
-  delay(100);
-  return 0;
-}
 //Menu principal du boitier.
 int  mainMenu() {
   actualMenu = 1;
   display.clearDisplay();
   display.setCursor(0, 0);
-  display.println(F("Boitier modulaire"));
+  display.println(F("Boitier de capteurs"));
   display.setCursor(0, 10);
   display.println(F("<= capteurs"));
   display.setCursor(65, 20);
@@ -71,67 +90,46 @@ int  mainMenu() {
 }
 
 //Menu catpeur du boitier.
-int sensorMenu() {
-  actualMenu = 0;
+int capteurMenu() {
+  actualMenu = 2;
+  //nbCapt=scanCapteur();
+  int nbCapt=1;
+  char charCapt = nbCapt;
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println(F(" Capteurs disponible")); 
-  display.setCursor(0, 10);
-  display.println(F("<= cadence"));
-  display.setCursor(20, 20);
-  display.println(F("menu principal =>")); 
+  display.setTextSize(2); 
+  display.setCursor(50, 10);
+  //display.println(F(charCapt)); 
   display.display();
   delay(100);
   return 0;
 }
 
 //Menu actualisation pour regeler la frequence de recuperation des données. 
-int rateMenu() {
+int actualisationMenu() {
   actualMenu = 2;    
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(F("Cadence")); 
-  display.setCursor(0, 10);
-  display.println(F("<= menu principal"));
-  display.setCursor(60, 20);
-  display.println(F("capteurs =>")); 
-  display.display();
   delay(100);
   return 0;
 }
-
-void changeMenu(int id){
-	delay(300);
-	switch (id){
-		case 0:
-			sensorMenu();
-			break;
-		case 1:
-			mainMenu();
-			break;
-		case 2:
-			rateMenu();
-			break;
-		default:
-			break;
-	}
-
-}
+///////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   if(!setOled())
 	Serial.println("Oled failed to connect");
+  delay(2000); 
   display.clearDisplay();
-  creditMenu();
-  delay(2000);
   mainMenu();
+  delay(1000);
+  capteurMenu();
+  delay(1000);
+  int nbc = scanCapteur(); 
+  Serial.println(nbc);
 }
 
 void loop() {
-	changeMenu(0);	
-	delay(3000);
-	changeMenu(1);	
-	delay(3000);
-	changeMenu(2);	
-	delay(3000);
+  delay(10000);
+  int nbc = scanCapteur(); 
+  Serial.println(nbc);
+
 }
